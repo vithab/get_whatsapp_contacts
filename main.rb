7220e2023ddd67325755823265c878768197ba6b
Vitaly Bogachev <vit.bogachev@gmail.com>
2021-05-31 12:09:57 +0300

    Get phone & name contact

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
 
+contacts = []
+
 file = File.open('./html/data.html', 'r') { |file| file.readline }
 
 document = Nokogiri::HTML(file)
@@ -7,6 +9,9 @@ document = Nokogiri::HTML(file)
 divs = document.css('div._2aBzC')
 
 divs.each do |div|
-  p div.css('div.TbtXF span.N2dUK').text
-  puts '='*20
+  phone = div.css('div.TbtXF span.N2dUK').text
+  name = div.css('div.TbtXF div._15smv span._3-8er').text
+  contacts.push("#{phone} - #{name}")
 end
+
+contacts.map { |c| puts c }
